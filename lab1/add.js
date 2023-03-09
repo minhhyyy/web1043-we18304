6ee43f0f0bf447ded363488acf9a9589f93252ed
minhhyyy <hyhmpd07975@fpt.edu.vn>
2023-03-09 13:37:32 +0700

    nop lab

---
+++ lab1/add.js
@@ -0,0 +1,7 @@
+//nhập a từ bàn phímphím
+let a = prompt("Nhap so nguyen a: ");
+// nhập b từ bàn phím
+let b = prompt("Nhap so nguyen b:");
+document.write(c);
+//show giá trị biến
+console.log(c);
