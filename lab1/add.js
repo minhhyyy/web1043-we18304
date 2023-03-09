6e86e587b8f4d7780945c9d20e36190341d8fe31
minhhyyy <hyhmpd07975@fpt.edu.vn>
2023-03-09 18:51:35 +0700

    nop lab

--- lab1/add.js
+++ lab1/add.js
@@ -2,6 +2,7 @@
 let a = prompt("Nhap so nguyen a: ");
 // nhập b từ bàn phím
 let b = prompt("Nhap so nguyen b:");
+let c = parseInt(a)+parseInt(b);
 document.write(c);
 //show giá trị biến
 console.log(c);
